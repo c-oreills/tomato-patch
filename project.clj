e197f58111255f51cf556e52751859087ebaef61
Christy O'Reilly <christy@oreills.co.uk>
2014-11-09 13:48:52 +0000

    Update core.async

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.6.5"]
                  [reagent "0.4.3"]
                  [garden "1.1.3"]
